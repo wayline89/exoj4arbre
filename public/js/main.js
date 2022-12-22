38a0204683a219fc5a194d1cebe0fcab561e434e
wayline89 <laurentdugardin@gmail.com>
2022-12-22 14:31:20 +0100

    exo3j4

---
+++ public/js/main.js
@@ -0,0 +1,28 @@
+
+// // exo 1111111
+
+
+// for (let i = 1; i <= 10; i++) {
+//     let tree = "";
+//     for (let j = 0; j < i; j++) {
+//       tree += "1";
+//     }
+//     console.log(tree);
+//   }
+
+
+// //   Ecrire un programme qui demande à l'utilisateur d'introduire un nombre
+// //   entier positif.
+// //   Le programme affiche la table de multiplication de ce nombre.
+
+
+// let num = +prompt("Entrez un nombre entier positif:");
+
+// if (num > 0 && Number.isInteger(num)) {
+
+//   for (let i = 1; i <= 100; i++) {
+//     console.log(`${num} x ${i} = ${num * i}`);
+//   }
+// } else {
+//   console.log("Vous n'avez pas entré un nombre entier positif.");
+// }
